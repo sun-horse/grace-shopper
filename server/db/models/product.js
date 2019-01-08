df84002879c97c7e6b523429912d42f7c51fef61
VandyWebDev <39500384+VandyWebDev@users.noreply.github.com>
2019-01-08 13:56:20 -0500

    define product model

---
+++ server/db/models/product.js
@@ -0,0 +1,30 @@
+const Sequelize = require('sequelize')
+const db = require('..')
+const DEFAULT_IMAGE_URL =
+  'https://image.spreadshirtmedia.com/image-server/v1/mp/designs/12644108,width=178,height=178/glitter-horse.png'
+
+const Product = db.define('products', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  price: {
+    type: Sequelize.DECIMAL,
+    allowNull: false,
+    validate: {
+      notEmpty: true
+    }
+  },
+  imageUrl: {
+    type: Sequelize.STRING,
+    defaultValue: DEFAULT_IMAGE_URL
+  },
+  description: {
+    type: Sequelize.TEXT
+  }
+})
+
+module.exports = Product
