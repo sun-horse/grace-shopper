f5dc182fbb6d94ed1c718015f6a129c899bace08
Eve Mendelevich <ym9382@gmail.com>
2019-01-10 11:30:53 -0500

    test: added data to beforeEach hook of orders.spec

--- server/db/models/order.spec.js
+++ server/db/models/order.spec.js
@@ -2,21 +2,29 @@ const {expect} = require('chai')
 const db = require('../index')
 const Order = db.model('orders')
 const OrderProducts = db.model('order-products')
+const User = db.model('users')
+const Product = db.model('products')
 
 describe('Order', () => {
   beforeEach(async () => {
-    const firstOrder = await Order.create({
-      userId: 1,
-      isActive: false,
-      finalizedAt: new Date()
-    })
+    const users = await Promise.all([
+      User.create({email: 'cody@email.com', password: '123'}),
+      User.create({email: 'murphy@email.com', password: '123'})
+    ])
+    const products = await Promise.all([
+      Product.create({name: 'glitter paint', price: 690}),
+      Product.create({name: 'soothing balm', price: 350}),
+      Product.create({name: 'flying dust', price: 1000})
+    ])
 
-    const firstOrderProducts = await OrderProducts.create({
-      orderId: 1,
-      userId: 1,
-      quantity: 5,
-      productId: 1
-    })
+    // Seed association tables
+    const newOrder = await Order.create({isActive: false})
+
+    const newUser = await User.findById(2)
+    await newUser.addOrders(newOrder)
+
+    const newProduct = await Product.findById(3)
+    await newProduct.addOrders(newOrder)
   })
 
   describe('Instance method `getTotal`', () => {
