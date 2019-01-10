1de47914bda80e7c7000ed9abad70ea4ce639756
Brynn Shepherd <brynn.shepherd@gmail.com>
2019-01-10 16:02:25 -0500

    refactor: handleAddToCartClick -> handleAddToCartSubmit

--- client/components/ProductCard.js
+++ client/components/ProductCard.js
@@ -22,13 +22,18 @@ export class ProductCard extends Component {
     this.state = {
       quantity: Number(this.props.product.quantity) || 1
     }
-    this.handleAddToCartClick = this.handleAddToCartClick.bind(this)
+    this.handleAddToCartSubmit = this.handleAddToCartSubmit.bind(this)
   }
 
-  handleAddToCartClick() {
-    // need to also send product quantity (this.state.quantity) to cart reducer
+  handleAddToCartSubmit(evt) {
+    evt.preventDefault()
+    const quantity = Number(evt.target.quantity.value)
     const productToAdd = this.props.product
-    this.props.addItem(productToAdd)
+    // for now, addItem doesn't accept a quantity
+    // we will eventually want to send the quantity to the cart reducer
+    for (let i = 0; i < quantity; i++) {
+      this.props.addItem(productToAdd)
+    }
   }
 
   render() {
@@ -38,29 +43,26 @@ export class ProductCard extends Component {
         <img src={product.imageUrl} height="200" width="200" />
         <h4>{name}</h4>
         <p>${formatPrice(product.price)}</p>
-        <div className="cart-item-quantity">
-          Quantity:{' '}
-          <select
-            name="item-quantity"
-            data-product-id={product.id}
-            onChange={handleQuantitySelect}
-            defaultValue={this.state.quantity}
-          >
-            <option value={1}>1</option>
-            <option value={2}>2</option>
-            <option value={3}>3</option>
-            <option value={4}>4</option>
-            <option value={5}>5</option>
-          </select>
-        </div>
-        <button
-          className="add-to-cart"
-          name="add"
-          onClick={this.handleAddToCartClick}
-          type="button"
-        >
-          + cart
-        </button>
+        <form method="post" onSubmit={this.handleAddToCartSubmit}>
+          <div className="cart-item-quantity">
+            Quantity:{' '}
+            <select
+              name="quantity"
+              data-product-id={product.id}
+              onChange={handleQuantitySelect}
+              defaultValue={this.state.quantity}
+            >
+              <option value={1}>1</option>
+              <option value={2}>2</option>
+              <option value={3}>3</option>
+              <option value={4}>4</option>
+              <option value={5}>5</option>
+            </select>
+          </div>
+          <button className="add-to-cart" name="add" type="submit">
+            + cart
+          </button>
+        </form>
       </div>
     )
   }
