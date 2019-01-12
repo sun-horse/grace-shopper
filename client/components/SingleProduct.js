ffbccc67dea5520eb50fdc7d9ec3163e9742b54e
VandyWebDev <39500384+VandyWebDev@users.noreply.github.com>
2019-01-12 17:53:00 -0500

    feat: SingleProduct component before adding render method

--- client/components/SingleProduct.js
+++ client/components/SingleProduct.js
@@ -2,16 +2,22 @@
 import {Component} from 'react'
 import {connect} from 'react-redux'
 import {formatPrice} from '../utils'
+import {getProduct} from '../store'
 
 // define class SingleProduct
-export class SingleProduct extends Component {}
+export class SingleProduct extends Component {
+  componentDidMount() {
+    const productId = Number(this.props.match.params.productId)
+    this.props.getProduct(productId)
+  }
+}
 
 const mapStateToProps = state => ({
   product: state.product
 })
 
 const mapDispatchToProps = dispatch => ({
-  // getProduct: dispatch(getProduct(productId))
+  getProduct: productId => dispatch(getProduct(productId))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(SingleProduct)
