aef22ec44e712ced0c2bcada24e32a7b975bd66f
Eve Mendelevich <ym9382@gmail.com>
2019-01-14 10:36:02 -0500

    feat: replaced navbar with bulma placeholder

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -13,35 +13,60 @@ export const Navbar = ({handleClick, isLoggedIn, cart}) => {
   totalProducts = totalProducts.toString()
 
   return (
-    <div>
-      <h1 className="title is-2">
-        <Link to="/products">Sun Horse</Link>
-      </h1>
-      <nav>
-        {isLoggedIn ? (
-          <div>
-            {/* The navbar will show these links after you log in */}
-            <Link to="/home">Home</Link>
-            <a href="#" onClick={handleClick}>
-              Logout
-            </a>
+    <nav className="navbar" role="navigation" aria-label="main navigation">
+      <div className="navbar-brand">
+        <a className="navbar-item" href="https://bulma.io">
+          <img
+            src="https://bulma.io/images/bulma-logo.png"
+            width="112"
+            height="28"
+          />
+        </a>
+
+        <a
+          role="button"
+          className="navbar-burger burger"
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarBasicExample"
+        >
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+          <span aria-hidden="true" />
+        </a>
+      </div>
+
+      <div id="navbarBasicExample" className="navbar-menu">
+        <div className="navbar-start">
+          <a className="navbar-item">Home</a>
+
+          <a className="navbar-item">Documentation</a>
+
+          <div className="navbar-item has-dropdown is-hoverable">
+            <a className="navbar-link">More</a>
+
+            <div className="navbar-dropdown">
+              <a className="navbar-item">About</a>
+              <a className="navbar-item">Jobs</a>
+              <a className="navbar-item">Contact</a>
+              <hr className="navbar-divider" />
+              <a className="navbar-item">Report an issue</a>
+            </div>
           </div>
-        ) : (
-          <div>
-            {/* The navbar will show these links before you log in */}
-            <Link to="/login">Login</Link>
-            <Link to="/signup">Sign Up</Link>
+        </div>
+
+        <div className="navbar-end">
+          <div className="navbar-item">
+            <div className="buttons">
+              <a className="button is-primary">
+                <strong>Sign up</strong>
+              </a>
+              <a className="button is-light">Log in</a>
+            </div>
           </div>
-        )}
-        <Link to="/cart">
-          <button type="button">
-            <i className="fas fa-shopping-cart" />
-            <p id="cart-count">{totalProducts}</p>
-          </button>
-        </Link>
-      </nav>
-      <hr />
-    </div>
+        </div>
+      </div>
+    </nav>
   )
 }
 
