202f956f7bdffc51bd759979c2573a6144afa1e3
Emily Quinn <emkquinn@gmail.com>
2019-01-15 12:18:43 -0500

    fix: removed unneeed quantity update action creator

--- client/store/cart.js
+++ client/store/cart.js
@@ -1,11 +1,10 @@
+import axios from 'axios'
+
 /**
  * ACTION TYPES
  */
 const ADD_ITEM = 'ADD_ITEM'
-const UPDATE_ITEM_QUANTITY = 'UPDATE_ITEM_QUANTITY'
 const GET_CART = 'GET_CART'
-
-import axios from 'axios'
 
 /**
  * INITIAL STATE
@@ -19,8 +18,6 @@ const defaultCart = {
  * ACTION CREATORS
  */
 const addItem = item => ({type: ADD_ITEM, item})
-export const updateItemQuantity = () => ({})
-
 const getCart = cart => ({type: GET_CART, cart, orderId: cart.orderId})
 
 /**
@@ -60,8 +57,6 @@ export default function(state = defaultCart, action) {
   switch (action.type) {
     case ADD_ITEM:
       return {...state, products: [...state.products, action.item]}
-    case UPDATE_ITEM_QUANTITY:
-      return state
     case GET_CART:
       return action.cart
     default:
