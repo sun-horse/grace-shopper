c3e4fe65baf95bf2c83f1c935d481acd8a696f88
Brynn Shepherd <brynn.shepherd@gmail.com>
2019-01-15 14:42:54 -0500

    chore: merge conflict fixes

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import {fetchProducts} from '../store'
+import {fetchProducts, setCart} from '../store'
 import {formatProductColumns} from '../utils'
 
 export class AllProducts extends Component {
