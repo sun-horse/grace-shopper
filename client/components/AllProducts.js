30111dc40444cc45e90a79432026088dc6050fa5
Brynn Shepherd <brynn.shepherd@gmail.com>
2019-01-15 13:13:43 -0500

    fix: dont chunk products if there are less than 3

--- client/components/AllProducts.js
+++ client/components/AllProducts.js
@@ -1,8 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
-import _ from 'lodash'
 import {fetchProducts} from '../store'
-import {ProductCard} from '.'
+import {formatProductColumns} from '../utils'
 
 export class AllProducts extends Component {
   componentDidMount() {
@@ -11,22 +10,10 @@ export class AllProducts extends Component {
 
   render() {
     const products = this.props.products
-    const productsInColumns = _.chunk(products, 2)
-
     return (
-      <div>
+      <div className="all-products">
         <h3 className="title is-2">All Products</h3>
-        <div className="columns">
-          {productsInColumns.map((column, i) => {
-            return (
-              <div className="column" key={i}>
-                {column.map(product => {
-                  return <ProductCard product={product} key={product.id} />
-                })}
-              </div>
-            )
-          })}
-        </div>
+        {formatProductColumns(products)}
       </div>
     )
   }
